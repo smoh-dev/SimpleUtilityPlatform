521bf97754a6cce351cb2bb861a8e41f85065122
smoh <seungmuk92@gmail.com>
2024-06-13 14:00:06 +0900

    #8 KMS 반영

--- Sup.Np.PageFixer/Program.cs
+++ Sup.Np.PageFixer/Program.cs
@@ -1,3 +1,4 @@
+using Sup.Common.Kms;
 using Sup.Common.TokenManager;
 using Sup.Np.PageFixer;
 
@@ -9,6 +10,45 @@ const string configFileName = "appsettings.json";
 
 var builder = Host.CreateApplicationBuilder(args);
 builder.Configuration.AddJsonFile(configFileName, optional: true, reloadOnChange: true);
+
+
+// Check license
+var apiUrl = builder.Configuration["ApiUrl"];
+var licenseKey = builder.Configuration["LicenseKey"];
+if (string.IsNullOrEmpty(apiUrl) || string.IsNullOrEmpty(licenseKey)) 
+    throw new Exception("Information required for licence validation is missing.");
+var license = await ProgramStartup.CheckLicenseAsync(apiUrl, licenseKey);
+if (license is not { Success: true })
+    throw new Exception("License is invalid.");
+
+
+// Add AWS KMS client
+var awsOptions = builder.Configuration.GetSection("AWS").Get<AwsKmsOptions>();
+if(awsOptions == null) throw new Exception("AWS configs are missing.");
+awsOptions.KeyId = license.KeyId;
+if(!awsOptions.IsValid()) throw new Exception("AWS configs are invalid.");
+builder.Services.AddSingleton<AwsKmsEncryptor>(_=> new AwsKmsEncryptor(awsOptions));
+
+
+// Register TokenManager.
+var decryptedSigningKey = "";
+using (var scope = builder.Services.BuildServiceProvider().CreateScope())
+{
+    var kmsEncryptor = scope.ServiceProvider.GetRequiredService<AwsKmsEncryptor>();
+    decryptedSigningKey = kmsEncryptor.DecryptStringAsync(license.SigningKey).Result;
+}
+builder.Services.AddSingleton<TokenManager>(_ =>
+{
+    var configs = new AuthConfigs
+    {
+        TokenUrl = license.TokenUrl,
+        Audience = license.Audience,
+        SigningKey = decryptedSigningKey
+    };
+    builder.Configuration.GetSection("OAuth").Bind(configs);
+    return new TokenManager(configs);
+});
+
 
 // The BackgroundWorker works as a singleton, so it instantiates and
 // uses the classes it needs internally without the need to DI.
